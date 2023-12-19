4b8e01266f145ce7e4a93f94bcddc2b0df156320
TanakaAkihiro0930 <redolution0930@gmail.com>
2023-12-19 23:38:47 +0900

    second

--- todo-backend/src/controllers/todos.controller.js
+++ todo-backend/src/controllers/todos.controller.js
@@ -15,7 +15,9 @@ exports.getAllTodos = async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   } catch (error) {
+
     res.status(500).json({ error: error.message });
+  
   }
 };
 
